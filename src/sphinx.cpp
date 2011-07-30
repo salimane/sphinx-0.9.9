4ed443365433dc73c69bbddc6c10c152f175a2a2
Salimane Adjao Moustapha <salimane@gmail.com>
2011-07-30 13:59:41 +0800

    fix new index not replacing old index on --rotate

--- src/sphinx.cpp
+++ src/sphinx.cpp
@@ -14386,6 +14386,7 @@ bool CSphIndex_VLN::Lock ()
 	{
 		m_sLastError.SetSprintf ( "failed to lock %s: %s", sName, strerror(errno) );
 		::close ( m_iLockFD );
+                m_iLockFD = -1;
 		return false;
 	}
 
